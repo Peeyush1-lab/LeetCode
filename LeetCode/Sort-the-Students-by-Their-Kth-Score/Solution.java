149547903e68a898a565c772896b8d822cfc0f8c
Peeyush Tiwari <115945243+Peeyush1-lab@users.noreply.github.com>
2025-07-20 20:38:08 +0530

    Add solution for Sort the Students by Their Kth Score

---
+++ LeetCode/Sort-the-Students-by-Their-Kth-Score/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[][] sortTheStudents(int[][] score, int k) {
+        int n = score.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (score[j][k] < score[j + 1][k]) {
+                    int[] temp = score[j];
+                    score[j] = score[j + 1];
+                    score[j + 1] = temp;
+                }
+            }
+        }
+
+        return score;
+    }
+}
