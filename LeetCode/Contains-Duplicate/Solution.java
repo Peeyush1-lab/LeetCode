169b798345e2d3f3c490af43defec391640a9ca7
Peeyush Tiwari <115945243+Peeyush1-lab@users.noreply.github.com>
2025-07-21 17:37:02 +0530

    Update Solution.java

--- LeetCode/Contains-Duplicate/Solution.java
+++ LeetCode/Contains-Duplicate/Solution.java
@@ -1,1 +1,11 @@
-var containsDuplicate = (nums) =>new Set(nums).size != nums.length;
+class Solution{
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> arr = new HashSet<>();
+        for (int i : nums) {
+            if(!arr.add(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
